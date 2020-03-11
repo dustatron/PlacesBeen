94e1e9f1bb75c47ce1c2cec2e69b0e4e11ce70e7
Student <example@example.com>
2020-03-11 09:57:47 -0700

    tests the instance made

--- PlacesBeen/Models/Places.cs
+++ PlacesBeen/Models/Places.cs
@@ -1,7 +1,32 @@
+using System.Collections.Generic;
+
 namespace PlacesBeen.Models
 {
-    public class Places
+  public class Places
+  {
+    private static List<Places> Trips = new List<Places>();
+
+    public string Name { get; set; }
+
+    public string Picture { get; set; }
+
+    public string StayLength { get; set; }
+
+    public string TravelTime { get; set; }
+
+    public Places(string name, string picture, string stayLength, string timeTravel)
     {
-        
+      Name = name;
+      Picture = picture;
+      StayLength = stayLength;
+      TravelTime = timeTravel;
+      Trips.Add(this);
     }
+    public static void ClearAll()
+    {
+      Trips.Clear();
+    }
+
+
+  }
 }
